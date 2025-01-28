4c66f86e522fb5de093999b723be88510e827053
LEPAPE-Lucas-24009890 <lucas.lepape.1@etu.univ-amu.fr>
2025-01-28 15:01:47 +0100

    Programme de test de la class Circle

---
+++ src/TestCircle.java
@@ -0,0 +1,8 @@
+
+public class TestCircle {
+	public static void main(String[] args) {
+		Circle cercle1 = new Circle();
+		System.out.println(cercle1.getRadius());
+		System.out.println(cercle1.getArea());
+	}
+}
