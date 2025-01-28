f8092b200dbbf964aa6867a3854ff3671706b837
LEPAPE-Lucas-24009890 <lucas.lepape.1@etu.univ-amu.fr>
2025-01-28 16:42:31 +0100

    Création de la class Employee + méthodes

---
+++ src/Employee.java
@@ -0,0 +1,47 @@
+
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int salary;
+	
+	public Employee(int id, String firstName, String lastName, int salary) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public int getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(int newSalary) {
+		salary = newSalary;
+	}
+	
+	public int getAnnualSalary() {
+		return 12*salary;
+	}
+	
+	public void raiseSalary(int percent) {
+		salary *= 1+percent/100;
+	}
+	
+	public String toString() {
+		return "Employee[Id=" + id + " FirstName =" + firstName + "Salary =" + salary + "]";
+	}
+	
+}
