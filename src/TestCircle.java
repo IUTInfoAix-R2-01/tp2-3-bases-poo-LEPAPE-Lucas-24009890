7cd754c064a1ca51e7de441a5f124d5c27cdc1b8
LEPAPE-Lucas-24009890 <lucas.lepape.1@etu.univ-amu.fr>
2025-01-28 17:15:41 +0100

    Modification du testCircle pour autre class

--- src/TestCircle.java
+++ src/TestCircle.java
@@ -1,13 +1,22 @@
-
 public class TestCircle {
-	public static void main(String[] args) {
-		Circle cercle1 = new Circle();
-		System.out.println(cercle1.getRadius());
-		System.out.println(cercle1.getArea());
-		System.out.println(cercle1.getColor());
-		cercle1.setColor("blue");
-		System.out.println(cercle1.getColor());
-		System.out.println(cercle1.toString());
-		System.out.println(cercle1.getCircumference());
-	}
+   public static void main(String[] args) {
+      // Test constructor and toString()
+      Employee e1 = new Employee(8, "Peter", "Tan", 2500);
+      System.out.println(e1);  // toString();
+
+      // Test Setters and Getters
+      e1.setSalary(999);
+      System.out.println(e1);  // toString();
+      System.out.println("id is: " + e1.getId());
+      System.out.println("firstname is: " + e1.getFirstName());
+      System.out.println("lastname is: " + e1.getLastName());
+      System.out.println("salary is: " + e1.getSalary());
+
+      System.out.println("name is: " + e1.getFirstName());
+      System.out.println("annual salary is: " + e1.getAnnualSalary()); // Test method
+
+      // Test raiseSalary()
+      System.out.println(e1.raiseSalary(10));
+      System.out.println(e1);
+   }
 }
